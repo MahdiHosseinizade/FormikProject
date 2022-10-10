3a6a2cfce00eeeab7fe236463383c32cf4c26f75
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2022-10-10 13:24:00 +0330

    password confrim

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,7 +4,9 @@ import * as Yup from 'yup' ;
 const value = {
     name:"",
     email:"",
-    password:""
+    password:"",
+    phoneNumber:"",
+    passwordConfrim:""
 }
 
 // const validatesError = (value) => {
@@ -15,13 +17,21 @@ const value = {
 //     return errors
 // }
 
+
 const validationschema = Yup.object({
     name : Yup.string().required('Name is Requierd'),
     email : Yup.string().email('Invalid email format').required('email is Requierd'),
-    password: Yup.string()
+    password: Yup.string().required('phoneNumber is Requierd')
     .required('Password is Requierd') 
     .min(8, 'Password is too short - should be 8 chars minimum.')
-    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+    phoneNumber:Yup.string()
+    .required('phoneNumber is Requierd')
+    .matches(/^[0-9]{11}$/,'Invalid Phone Number')
+    .nullable(),
+    passwordConfrim:Yup.string()
+    .required('password Confrimation is Requierd')
+    .oneOf([Yup.ref('password'),null],'password must match')
 })
 
 const SignUpForm = () => {
@@ -29,7 +39,8 @@ const SignUpForm = () => {
     const formik = useFormik({
         initialValues : value,
         onSubmit : (values) => console.log(values),
-        validationSchema : validationschema
+        validationSchema : validationschema,
+        validateOnMount:true,
         })
     // console.log(formik.handleSubmit);
     
@@ -56,6 +67,15 @@ const SignUpForm = () => {
                 />
                 {formik.errors.email && formik.touched.email  && <div className="error">{formik.errors.email}</div>}
             </div>
+            <div className="formControl">   
+                <label>phoneNumber</label>
+                <input 
+                    type="text"
+                    name = "phoneNumber"
+                    {...formik.getFieldProps('phoneNumber')}
+                />
+                {formik.errors.phoneNumber && formik.touched.phoneNumber &&  <div className="error">{formik.errors.phoneNumber}</div>}
+            </div>
             <div className="fromControl">
                 <label>password</label>
                 <input 
@@ -65,7 +85,20 @@ const SignUpForm = () => {
                 />
                 {formik.errors.password && formik.touched.password  && <div className="error">{formik.errors.password}</div>}
             </div>
-            <button className="submit-button" type="submit">Submit</button>
+            <div className="formControl">   
+                <label>password confrim</label>
+                <input 
+                    type="password"
+                    name = "passwordConfrim"
+                    {...formik.getFieldProps('passwordConfrim')}
+                />
+                {formik.errors.passwordConfrim && formik.touched.passwordConfrim &&  <div className="error">{formik.errors.passwordConfrim}</div>}
+            </div>
+            <button 
+                className="submit-button"
+                type="submit"
+                disabled={!formik.isValid}
+                >Submit</button>
         </form>
     );
 }
